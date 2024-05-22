32bc4d85fbaa47886a5e6d7508462028bcdca4d1
joaoleandro <new.joaoleandro@gmail.com>
2024-05-22 13:47:29 -0300

    chore: add event keydown in Input Header

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -7,6 +7,7 @@ import { Input } from "./ui/input";
 import { AuthContext } from "@/context/auth";
 import { useContext, useState } from "react";
 import { Search } from "lucide-react";
+import { useRouter } from 'next/navigation'
 
 const HeaderBar = () => {
 
@@ -16,7 +17,19 @@ const HeaderBar = () => {
   const handleClick = () => {
     if(inputChange === "") return 
     setInputValue(inputChange)
+    setInputChange("")
   }
+
+  const router = useRouter()
+  const keyDownEnter = (e: any) => {
+    if (e.key === 'Enter') {
+      setInputValue(inputChange)
+      setInputChange("")
+      router.push("/categories/search")
+      return
+    }
+  }
+
   return (
     <div className="border-b border-white bg-zinc-200/20 shadow-lg sticky top-0 z-50">
       <Container>
@@ -29,9 +42,12 @@ const HeaderBar = () => {
 
           <div className="relative">
           <Input
+              autoFocus
               className="md:w-[500px] sm:w-[400px] w-[190px] bg-slate-300"
               placeholder="Procure aqui..."
               onChange={(e) => setInputChange(e.target.value)}
+              onKeyDown={keyDownEnter}
+              value={inputChange}
             />
             <Link href={"/categories/search"} onClick={handleClick}><Search className="w-5 h-5 absolute top-2 right-3 hover:scale-105 transition duration-200" /></Link>
           </div>
